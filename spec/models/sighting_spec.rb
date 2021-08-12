5451703cfc3642300f6d985e63b9d9a5a9bcc904
hroulston <h_roulston@live.com>
2021-08-12 16:52:21 -0700

    forgot to save everything

--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Sighting, type: :model do
+
   it 'it is something' do
     # newsighting = Sighting.create(date:"2021-08-12 21:50:21.817689", latitude:"8.7832°S", longitude:"34.5085°E")
     expect{Sighting.new}.to_not raise_error
